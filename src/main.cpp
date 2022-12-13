401340123478b121b27e128bd398179567565dce
SirAlbaToR <90750816+SirAlbaToR@users.noreply.github.com>
2022-12-13 21:15:34 +0300

    new transmitter

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,43 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
-#include "string.h"
+#include <nRF24L01.h>
+#define PIN_CE 9
+#define PIN_CSN 10
 
-RF24 radio(9,10);
-const byte address[6] = "00001";
+RF24 radio(PIN_CE, PIN_CSN);
+const int TRANSMITTER_BUFFER_SIZE = 32;
+String receivedString;
+char buffer[TRANSMITTER_BUFFER_SIZE+1];
+int receiving = 0;
+char symbol_start = 1;
+char symbol_stop = 3;
 
-void setup()
-{
-    while (!Serial);
-    Serial.begin(9600);
+void setup(){
     radio.begin();
-    radio.openReadingPipe(0, address);
-    radio.startListening  ();
+    radio.setDataRate(RF24_1MBPS);
+    radio.setPALevel(RF24_PA_HIGH);
+    radio.openReadingPipe(1, 0x7878787878LL);
+    radio.startListening();
+    Serial.begin(9600);
 }
 
-void loop()
-{
-    if (radio.available())
-    {
-        std::string output;
-        while(radio.available()){
-            char trash[2];
-            radio.read(&trash, sizeof(1));
-            if(trash[0] = char(1)) break;
-        }
-            while(radio.available()){
-                char checkSymbol[2];
-                radio.read(&checkSymbol, sizeof(1));
-                if(checkSymbol[0] = char(1)) break;
-                output+=checkSymbol[0];
+void loop() {
+    if (radio.available() > 0) {
+        if(receiving == 1){
+            radio.read(&buffer, TRANSMITTER_BUFFER_SIZE);
+            if (buffer[0] == symbol_stop){
+                receiving = 0;
+                Serial.println(receivedString);
+            } else {
+                if (strlen(buffer) > 0){
+                    receivedString += String(buffer);
+                }
+            }
+        } else {
+            radio.read(&buffer, TRANSMITTER_BUFFER_SIZE);
+            if (buffer[0] == symbol_start) {
+                receiving = 1;
+                receivedString = "";
             }
         }
-    Serial.println(output);
     }
+}
